de025aecbc4fb97832400634223d37107e45f803
nomadhuns <kimalss22@gmail.com>
2023-06-29 15:51:04 +0900

    상품 상세 보기 Mock 구현 및 테스트 완료

--- src/main/java/com/example/kakaoshop/product/ProductRestController.java
+++ src/main/java/com/example/kakaoshop/product/ProductRestController.java
@@ -1,9 +1,13 @@
 package com.example.kakaoshop.product;
 
 import com.example.kakaoshop._core.utils.ApiUtils;
+import com.example.kakaoshop.product.response.OptionDTO;
 import com.example.kakaoshop.product.response.ProductRespFindAllDTO;
+import com.example.kakaoshop.product.response.ProductRespFindByIdDTO;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
@@ -48,4 +52,38 @@ public class ProductRestController {
 
         return ResponseEntity.ok().body(ApiUtils.success(responseDTO));
     }
+
+    @GetMapping("/products/{id}")
+    public ResponseEntity<?> findById(@PathVariable int id) {
+        // 상품을 담을 DTO 생성
+        ProductRespFindByIdDTO responseDTO = null;
+
+        switch (id) {
+            case 1 : {
+                List<OptionDTO> optionDTOList = new ArrayList<>();
+                optionDTOList.add(new OptionDTO(1, "01. 슬라이딩 지퍼백 크리스마스에디션 4종", 10000));
+                optionDTOList.add(new OptionDTO(2, "02. 슬라이딩 지퍼백 플라워에디션 5종", 10900));
+                optionDTOList.add(new OptionDTO(3, "고무장갑 베이지 S(소형) 6팩", 9900));
+                optionDTOList.add(new OptionDTO(4, "뽑아쓰는 키친타올 130매 12팩", 16900));
+                optionDTOList.add(new OptionDTO(4, "2겹 식빵수세미 6매", 8900));
+                responseDTO = new ProductRespFindByIdDTO(1, "기본에 슬라이딩 지퍼백 크리스마스/플라워에디션 에디션 외 주방용품 특가전", "", "/images/1.jpg", 1000, 5, optionDTOList);
+                break;
+            }
+            case 2 : {
+                List<OptionDTO> optionDTOList = new ArrayList<>();
+                optionDTOList.add(new OptionDTO(6, "22년산 햇단밤 700g(한정판매)", 9900));
+                optionDTOList.add(new OptionDTO(7, "22년산 햇단밤 1kg(한정판매)", 14500));
+                optionDTOList.add(new OptionDTO(8, "밤깎기+다회용 구이판 세트", 5500));
+                responseDTO = new ProductRespFindByIdDTO(2, "[황금약단밤 골드]2022년산 햇밤 칼집밤700g외/군밤용/생율", "", "/images/2.jpg", 2000, 5, optionDTOList);
+            }
+        }
+
+        // 존재 하지 않는 상품
+        if (responseDTO == null) {
+            return ResponseEntity.badRequest().body(ApiUtils.error("해당 상품을 찾을 수 없습니다 : " + id, HttpStatus.BAD_REQUEST));
+        }
+
+        return ResponseEntity.ok(ApiUtils.success(responseDTO));
+    }
+
 }
--- src/test/java/com/example/kakaoshop/product/ProductRestControllerTest.java
+++ src/test/java/com/example/kakaoshop/product/ProductRestControllerTest.java
@@ -41,4 +41,34 @@ public class ProductRestControllerTest {
         resultActions.andExpect(jsonPath("$.response[1].image").value("/images/2.jpg"));
         resultActions.andExpect(jsonPath("$.response[1].price").value(2000));
     }
+
+    @Test
+// 개별 상품 상세 조회
+    public void findById_test() throws Exception {
+        // given
+        int id = 1;
+
+        // when
+        ResultActions resultActions = mvc.perform(
+                get("/products/" + id)
+        );
+
+        String responseBody = resultActions.andReturn().getResponse().getContentAsString();
+        System.out.println("테스트 : " + responseBody);
+
+        // verify
+        resultActions.andExpect(jsonPath("$.success").value("true"));
+        resultActions.andExpect(jsonPath("$.response.id").value(1));
+        resultActions.andExpect(jsonPath("$.response.productName").value("기본에 슬라이딩 지퍼백 크리스마스/플라워에디션 에디션 외 주방용품 특가전"));
+        resultActions.andExpect(jsonPath("$.response.description").value(""));
+        resultActions.andExpect(jsonPath("$.response.image").value("/images/1.jpg"));
+        resultActions.andExpect(jsonPath("$.response.price").value(1000));
+        resultActions.andExpect(jsonPath("$.response.options[0].id").value(1));
+        resultActions.andExpect(jsonPath("$.response.options[0].optionName").value("01. 슬라이딩 지퍼백 크리스마스에디션 4종"));
+        resultActions.andExpect(jsonPath("$.response.options[0].price").value(10000));
+        resultActions.andExpect(jsonPath("$.response.options[1].id").value(2));
+        resultActions.andExpect(jsonPath("$.response.options[1].optionName").value("02. 슬라이딩 지퍼백 플라워에디션 5종"));
+        resultActions.andExpect(jsonPath("$.response.options[1].price").value(10900));
+    }
+
 }
